cba801742afab5a54ff4b2a1b5c57d5622ac3185
Magesh90 <mageshb@live.com>
2016-02-14 23:34:47 -0600

    first commit

---
+++ src/main/java/com/controllers/RequestHandlingController.java
@@ -0,0 +1,22 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RequestHandlingController {
+
+	String message = "Loading Controllers";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String readthis) {
+		System.out.println("inside hello controller");
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", readthis);
+		return mv;
+	}
+}
---
+++ src/main/java/com/views/model/Registration.java
@@ -0,0 +1,22 @@
+package com.views.model;
+
+public class Registration {
+	private String userName;
+	private String UserMail;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserMail() {
+		return UserMail;
+	}
+
+	public void setUserMail(String userMail) {
+		UserMail = userMail;
+	}
+}
